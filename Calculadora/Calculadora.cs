187522b47c80305e5f67de06ed305c22be728ea5
Navarro Walter <navarrowalterd@gmail.com>
2026-05-26 15:54:35 -0300

    Inicio calculadora, add gitignore

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,32 @@
+using System.Dynamic;
+
+namespace EspacioCalculadora
+{    
+    class Calculadora
+    {
+        int dato = 0;
+
+    public void Sumar(double termino)
+        {
+            
+        }
+    public void Restar(double termino)
+        {
+            
+        }
+    public void Multiplicar(double termino)
+        {
+            
+        }
+    public void Dividir(double termino)
+        {
+            
+        }
+    public void Limpiar()
+        {
+            
+        }
+
+    }
+}
+
