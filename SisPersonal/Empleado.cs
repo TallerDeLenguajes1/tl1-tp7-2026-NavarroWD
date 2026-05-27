6623bf93f94fd50a395b9e8f03227a5af7408e50
Navarro Walter <navarrowalterd@gmail.com>
2026-05-27 17:40:38 -0300

    Ejercicio 2

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace EspacioEmpleado
 {
     
@@ -12,13 +14,80 @@ namespace EspacioEmpleado
 
     public class Empleado
     {
-        private string nombre;
-        private string apellido;
-        private DateTime fechaNacimiento;
-        private char estadoCivil;
-        private DateTime fechaIngreso;
-        private double sueldoBasico;
-        private Cargos cargo;
+        public string Nombre {get; set;}
+        public string Apellido {get; set;}
+        public DateTime FechaNacimiento {get; set;}
+        public char EstadoCivil {get; set;}
+        public DateTime FechaIngreso {get; set;}
+        public double SueldoBasico {get; set;}
+        public Cargos Cargo {get; set;}
 
+        public Empleado(
+            string nombre,
+            string apellido,
+            DateTime fechaNacimiento,
+            char estadoCivil,
+            DateTime fechaIngreso,
+            double sueldoBasico,
+            Cargos cargo
+        )
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNacimiento = fechaNacimiento;
+            EstadoCivil = estadoCivil;
+            FechaIngreso = fechaIngreso;
+            SueldoBasico = sueldoBasico;
+            Cargo = cargo;
+        }
+        public int calcularEdad()
+        {
+            int edad = 2026 - FechaNacimiento.Year;
+            return edad;
+        }
+
+        public int calcularAntiguedad()
+        {
+            int antiguedad = 2026 - FechaIngreso.Year;
+            return antiguedad;
+        }
+
+        public int calcularJubilacion()
+        {
+            int faltante = 65 - calcularEdad();
+            return faltante;
+        }        
+
+        public double calcularAdicional()
+        {
+            double adicional = 0;
+            int antiguedad = calcularAntiguedad();
+
+            if(antiguedad <= 20)
+            {
+                adicional = SueldoBasico * antiguedad * 0.01;
+            }
+            else
+            {
+                adicional = SueldoBasico * 0.25;
+            }
+
+            if(Cargo == Cargos.Ingeniero || Cargo == Cargos.Especialista)
+            {
+                adicional = adicional * 1.5;
+            }    
+
+            if(EstadoCivil == 'C' || EstadoCivil == 'c')
+            {
+                adicional = adicional + 150000;
+            }
+            
+            return adicional;
+        }
+        public double CalcularSalario()
+        {
+            double salario = SueldoBasico + calcularAdicional();
+            return salario;
+        }
     }
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,3 +1,59 @@
-﻿using espacioEmpleado;
+﻿using EspacioEmpleado;
 
-Empleado empleado1 = new Empleado();
+Empleado[] empleados = new Empleado[3];
+
+
+empleados[0] = new Empleado(
+    "Juan",
+    "Perez",
+    new DateTime(1987, 5, 13),
+    'S',
+    new DateTime(2022, 3, 1),
+    1300000,
+    Cargos.Ingeniero
+);
+
+empleados[1] = new Empleado(
+    "Jose",
+    "Diaz",
+    new DateTime(1976, 3, 3),
+    'C',
+    new DateTime(2012, 2, 11),
+    950000,
+    Cargos.Administrativo
+);
+
+empleados[2] = new Empleado(
+    "Maria",
+    "Aguero",
+    new DateTime(1998, 5, 30),
+    'C',
+    new DateTime(2015, 11, 15),
+    1100000,
+    Cargos.Investigador
+);
+
+double totalSalarios = 0;
+
+foreach(Empleado emp in empleados)
+{
+    totalSalarios += emp.CalcularSalario();
+}
+
+System.Console.WriteLine("Total salarios: $" + totalSalarios);
+
+Empleado proximoJubilo = empleados[0];
+
+foreach(Empleado emp in empleados)
+{
+    if(emp.calcularJubilacion() < proximoJubilo.calcularJubilacion())
+    {
+        proximoJubilo = emp;
+    }
+}
+
+System.Console.WriteLine("Proximo jubilo: " + proximoJubilo.Apellido + " " + proximoJubilo.Nombre);
+System.Console.WriteLine("Edad: " + proximoJubilo.calcularEdad());
+System.Console.WriteLine("Salario: " + proximoJubilo.CalcularSalario());
+System.Console.WriteLine("Antiguedad: " + proximoJubilo.calcularAntiguedad());
+System.Console.WriteLine("Tiempo para jubilarse: " + proximoJubilo.calcularJubilacion());
